7006090d69496cffb398ce7d99e0677d8fdb0281
PTagoreBabu <135963543+PTagoreBabu@users.noreply.github.com>
2023-06-09 17:19:46 +0530

    Create server.tf

---
+++ server.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.64.0"
+    }
+  }
+}
+
+provider "aws" {
+region = "ap-south-1"
+}
+resource "aws_instance" "myawsserver" {
+  ami = "ami-0a3c8f9955a39dafb"
+  instance_type = "t2.micro"
+  key_name = "san"
+
+  tags = {
+    Name = "Nginx-server"
+    env = "Production"
+    owner = "Sandeep"
+  }
+  provisioner "local-exec" {
+    command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /etc/ansible/env"
+  }
+}
